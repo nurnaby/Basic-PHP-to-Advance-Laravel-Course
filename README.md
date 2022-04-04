79fcaefe33619520ead85463fd2f8d583928bf7e
nurnaby <sohag.biit@gmail.com>
2022-04-05 01:36:41 +0600

    class four assignment

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Basic-PHP-to-Advance-Laravel-Course
 Take your skills to a new level and join millions of users that have learned PHP and laravel
+![alt](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9rEj0XyCed8vWnBwHNX7jpjDc48WE3k-v5w&usqp=CAU)
--- class_4/index.php
+++ class_4/index.php
@@ -1,5 +1,12 @@
 <?php
+// Question 01 
 
+  $length = cal_days_in_month(CAL_GREGORIAN,4,2022);
+  for ($i=0; $i<=$length; $i++) {
+      echo $i;
+      echo "<br>";
+
+  }
 
 
 // Question 02: 
@@ -11,12 +18,30 @@
 //         }
 // }
 
-$x = 100;
+
+// Question 04: 
+// Print out from 100 - 1 using a while loop 
+
+// $x = 100;
  
-while($x >=1) {
-  echo "The number is: $x <br>";
-  $x--;
-}
+// while ($x >=1) {
+//   echo "The number is: $x <br>";
+//   $x--;
+// }
+
+
+// $day = cal_days_in_month(CAL_GREGORIAN, 4, 2022);
+// echo $day;
+
+
+// Question 05: 
+// $prosucts = ["apple", "orange","banana","mango" ];
+// foreach ($prosucts as $product) {
+    
+//     echo $product;
+    
+// }
+
 
 
 
