31a0ca358cc8236442807ecf5e74ff9a99427d5e
nurnaby <sohag.biit@gmail.com>
2022-04-04 21:41:29 +0600

    class four assignment

---
+++ class_4/index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+// Question 02: 
+// print out only the odd  number within 1-100 use for loop
+for ($i=1; $i<=100; $i++ ) {
+        if($i%2!==0){
+            echo $i." "."odd number";
+            echo "<br>";
+        }
+}
+
+
+
+
+
+
+
+?>
