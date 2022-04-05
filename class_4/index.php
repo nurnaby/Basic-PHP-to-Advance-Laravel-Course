f9235ca418d21eb423b4264e4ec0df55a6ca0893
nurnaby <sohag.biit@gmail.com>
2022-04-05 20:59:02 +0600

    class four assignment

--- class_4/index.php
+++ class_4/index.php
@@ -1,50 +1,55 @@
 <?php
 // Question 01 
-
-  // $length = cal_days_in_month(CAL_GREGORIAN,4,2022);
-  // for ($i=1; $i<=$length; $i++) {
-  //     echo $i;
-  //     echo "<br>";
-
-  // }
+  $length = cal_days_in_month(CAL_GREGORIAN,4,2022);
+  for ($i=1; $i<=$length; $i++) {
+      echo $i;
+      echo "<br>";
+  }
 
 
 // Question 02: 
 // print out only the odd  number within 1-100 use for loop
-// for ($i=1; $i<=100; $i++ ) {
-//         if($i%2!==0){
-//             echo $i." "."odd number";
-//             echo "<br>";
-//         }
-// }
+
+for ($i=1; $i<=100; $i++ ) {
+        if($i%2!==0){
+            echo $i." "."odd number";
+            echo "<br>";
+        }
+}
+
+// Question 03: 
+$num  = 7;
+$fact =1;
+for ($i =$num; $i>0; $i--) {
+    $fact = $fact * $i;
+}
+echo "factorial of $num is =" .$fact;
+echo"<br>";
+
+
+
+
+
 
 
 // Question 04: 
 // Print out from 100 - 1 using a while loop 
 
-// $x = 100;
- 
-// while ($x >=1) {
-//   echo "The number is: $x <br>";
-//   $x--;
-// }
-
-
-// $day = cal_days_in_month(CAL_GREGORIAN, 4, 2022);
-// echo $day;
-
+$x = 100;
+while ($x >=1) {
+  echo "The number is: $x <br>";
+  $x--;
+}
 
 // Question 05: 
 $prosucts = ["apple", "orange","banana","mango" ];
-
-
 echo"<table border='1px' cellpadding='5px' cellspacing='0'>";
 echo"<tr>
         <th>Sl</th>
         <th>Product Name</th>
     </tr>";
 foreach ($prosucts as $sl=> $product) {
-  $s=++$sl;
+        $s=++$sl;
     echo"<tr>";
         echo"<td> $s</td><td>$product</td>";
         
