5a91315347d491010a4ea3f8fb5c0019d5cbbb62
nurnaby <sohag.biit@gmail.com>
2022-04-16 04:19:16 +0600

    class six assignment

--- class_6/index.php
+++ class_6/index.php
@@ -13,22 +13,23 @@ $total_qut = array_sum(array_column($purchased_products, 'qty'));
 echo "Total amount: ".$total_amount."tk"."<br>";
 echo "Total purchased qty:".$total_qut;
 
-// // Question 02: 
-// $first_email_array = ["abc@gmail.com", "xyz@gmail.com", "demo@gmail.com"]; 
-// $second_email_array = ["efg@gmail.com", "xyz@gmail.com", "dummy@gmail.com"];
-// $array = array_unique(array_merge($first_email_array, $second_email_array));
+// Question 02: 
+$first_email_array = ["abc@gmail.com", "xyz@gmail.com", "demo@gmail.com"]; 
+$second_email_array = ["efg@gmail.com", "xyz@gmail.com", "dummy@gmail.com"];
+$array = array_unique(array_merge($first_email_array, $second_email_array));
 
-// echo "<pre>";
-// print_r($array);
-// echo "</pre>";
+echo "<pre>";
+print_r($array);
+echo "</pre>";
 
-// // Question 03:
+// Question 03:
 
-// $citylist = array( "Black Canyon City", "Chandler", "Flagstaff", "Gilbert", "Glendale", "Globe", "Mesa", "Miami", "Phoenix", "Peoria", "Prescott", "Scottsdale", "Sun City", "Surprise", "Tempe", "Tucson", "Wickenburg" );
+$citylist = array( "Black Canyon City", "Chandler", "Flagstaff", "Gilbert", "Glendale", "Globe", "Mesa", "Miami", "Phoenix", "Peoria", "Prescott", "Scottsdale", "Sun City", "Surprise", "Tempe", "Tucson", "Wickenburg" );
 
 
-
-// print_r($citylist);
+echo "<pre>";
+print_r(array_chunk($citylist,4));
+echo "</pre>";
 
 
 
