7070a996b58ed9d82097c75408c04977db1e3dbc
nurnaby <sohag.biit@gmail.com>
2022-04-21 13:52:52 +0600

    class 7 assignment

--- class_updat_7/index.php
+++ class_updat_7/index.php
@@ -72,6 +72,7 @@ $qusetionSet = [
 
 
 
+
 ?>
 
 <!doctype html>
@@ -81,21 +82,14 @@ $qusetionSet = [
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-
     <title>assignment 7</title>
 </head>
 
 <body>
-
-
-
-
-
     <section class="team">
         <div class="service-section pt-5">
             <h1 class="spantext">Our <span>Team Member</span> </h1>
@@ -105,9 +99,6 @@ $qusetionSet = [
             <div class="row">
                 <?php
                  foreach ($our_teams as $our_team) {
-                    //     echo $our_team['name'];
-                    
-                
                 ?>
                 <div class="col-lg-3 col-md-4 col-sm-3">
                     <div
@@ -130,21 +121,11 @@ $qusetionSet = [
                 </div>
                 <?php  }?>
 
-
-
-
-
-
-
-
-
             </div>
         </div>
     </section>
 
     <!-- cade section end -->
-
-
 
     <!-- Optional JavaScript; choose one of the two! -->
 
