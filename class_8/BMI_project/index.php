391cf11a3fd006b31a3831c60582ecc20c7d3ffa
nurnaby <sohag.biit@gmail.com>
2022-04-24 11:52:20 +0600

    class 8 assigment

---
+++ class_8/BMI_project/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <form action="" method="post">
+            <div class="haeding">
+                <h2>Check Your BMI</h2>
+            </div>
+            <div class="section1">
+                <span>Enter your Height:</span>
+                <input type="text" placeholder="Miter" name="height">
+            </div>
+            <div class="section2">
+                <span>Enter your Weight:</span>
+                <input type="text" placeholder="kilogram" name="weight">
+            </div>
+            <div class="section3">
+                <input type="submit" name="submit" value="Check BMI">
+                <input type="reset" value="clear">
+            </div>
+        </form>
+    </div>
+
+
+</body>
+
+</html>
