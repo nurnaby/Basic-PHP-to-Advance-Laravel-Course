34a3581cb023a3ec8b3322440efcc42b3929100c
nurnaby <sohag.biit@gmail.com>
2022-04-24 22:36:20 +0600

    class 8 assignment

---
+++ class_8/rectangle_square_triangle_function/index.php
@@ -0,0 +1,13 @@
+<?php
+
+function rectangle(){
+    $length = 5;
+    $width = 3;
+    $areaOfRectangle = $length * $width;
+    return $areaOfRectangle;
+}
+echo "area of a rectangle".rectangle()."<br>";
+
+
+
+?>
